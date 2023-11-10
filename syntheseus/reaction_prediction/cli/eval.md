985101416ed96141cdedb126b6ed4b7816875c14
Krzysztof Maziarz <krmaziar@microsoft.com>
2023-11-10 23:01:20 +0000

    Support *.csv and *.smi formats for the single-step evaluation data (#33)
    Currently, running single-step evaluation requires supplying data in our
    special `*.jsonl` format. This means that a user needs to understand how
    the format is structured, and then convert their existing data. Instead
    of adding a conversion script, this PR extends the `DiskReactionDataset`
    class to support two additional formats: a csv-based one, commonly used
    to store raw USPTO-50K data, and a raw list-of-reaction-SMILES format.
    By default the right format is selected automatically based on the files
    present in the given directory. This allows running evaluation using the
    raw USPTO-50K data available on the web as-is. Moreover, converting from
    another format to the JSONL-based one is now just a matter of loading
    the data through the `DiskReactionDataset` class and then re-saving.

---
+++ syntheseus/reaction_prediction/cli/eval.md
@@ -0,0 +1,51 @@
+# Single-step Evaluation
+
+## Usage
+
+To run single-step model evaluation, run
+
+```
+python ./retrosynthesis/reaction_prediction/cli/eval.py \
+    data_dir=[DATA_DIR] \
+    model_class=[MODEL_CLASS] \
+    model_dir=[MODEL_DIR]
+```
+
+The script accepts further arguments to customize the evaluation such as which data fold to use; see `BaseEvalConfig` for the complete list.
+
+The code will scan `data_dir` looking for files matching `*{train, val, test}.{jsonl, csv, smi}` and select the right data format based on the file extension. An error will be raised in case of ambiguity. Only the fold that was selected for evaluation has to be present.
+
+## Data format
+
+The single-step evaluation script supports reaction data in one of three formats.
+
+### JSONL
+
+Our internal format based on `*.jsonl` files in which each line is a JSON representation of a single reaction, for example:
+```json
+{"reactants": [{"smiles": "Cc1ccc(Br)cc1"}, {"smiles": "Cc1ccc(B(O)O)cc1"}], "products": [{"smiles": "Cc1ccc(-c2ccc(C)cc2)cc1"}]}
+```
+This format is designed to be flexible at the expense of taking more disk space. The JSON is parsed into a `ReactionSample` object, so it can include additional metadata such as template information, while the reactants and products can include other fields accepted by the `Molecule` object. The evaluation script will only use reactant and product SMILES to compute the metrics.
+
+Unlike the other formats below, reactants and products in this format are assumed to be already stripped of atom mapping, which leads to slightly faster data loading as that avoids extra calls to `rdkit`.
+
+### CSV
+
+This format is based on `*.csv` files and is commonly used to store raw USPTO data, e.g. as released by [Dai et al.](https://github.com/Hanjun-Dai/GLN):
+
+```
+id,class,reactants>reagents>production
+ID,UNK,[cH:1]1[cH:2][c:3]([CH3:4])[cH:5][cH:6][c:7]1Br.B(O)(O)[c:8]1[cH:9][cH:10][c:11]([CH3:12])[cH:13][cH:14]1>>[cH:1]1[cH:2][c:3]([CH3:4])[cH:5][cH:6][c:7]1[c:8]2[cH:14][cH:13][c:11]([CH3:12])[cH:10][cH:9]2
+```
+
+The evaluation script will look for the `reactants>reagents>production` column to extract the reaction SMILES, which are stripped of atom mapping and canonicalized before being fed to the model.
+
+### SMILES
+
+The most compact format is to list reaction SMILES line-by-line in a `*.smi` file:
+
+```
+[cH:1]1[cH:2][c:3]([CH3:4])[cH:5][cH:6][c:7]1Br.B(O)(O)[c:8]1[cH:9][cH:10][c:11]([CH3:12])[cH:13][cH:14]1>>[cH:1]1[cH:2][c:3]([CH3:4])[cH:5][cH:6][c:7]1[c:8]2[cH:14][cH:13][c:11]([CH3:12])[cH:10][cH:9]2
+```
+
+The data will be handled in the same way as for the CSV format, i.e. it will only be fed into model after removing atom mapping and canonicalization.
--- syntheseus/tests/reaction_prediction/data/test_dataset.py
+++ syntheseus/tests/reaction_prediction/data/test_dataset.py
@@ -1,20 +1,23 @@
+import csv
+from pathlib import Path
+
 import pytest
 
+from syntheseus.interface.bag import Bag
+from syntheseus.interface.molecule import Molecule
 from syntheseus.reaction_prediction.data.dataset import (
+    CSV_REACTION_SMILES_COLUMN_NAME,
     DataFold,
+    DataFormat,
     DiskReactionDataset,
-    ReactionDataset,
 )
 from syntheseus.reaction_prediction.data.reaction_sample import ReactionSample
 
 
 @pytest.mark.parametrize("mapped", [False, True])
-def test_save_and_load(tmp_path, mapped):
-    sample_cls = ReactionSample
-    sample_kwargs = {}
-
+def test_save_and_load(tmp_path: Path, mapped: bool) -> None:
     samples = [
-        sample_cls.from_reaction_smiles_strict(reaction_smiles, mapped=mapped, **sample_kwargs)
+        ReactionSample.from_reaction_smiles_strict(reaction_smiles, mapped=mapped)
         for reaction_smiles in [
             "O[c:1]1[cH:2][c:3](=[O:4])[nH:5][cH:6][cH:7]1>>[cH:1]1[cH:2][c:3](=[O:4])[nH:5][cH:6][cH:7]1",
             "CC(C)(C)OC(=O)[N:1]1[CH2:2][CH2:3][C@H:4]([F:5])[CH2:6]1>>[NH:1]1[CH2:2][CH2:3][C@H:4]([F:5])[CH2:6]1",
@@ -22,10 +25,81 @@ def test_save_and_load(tmp_path, mapped):
     ]
 
     for fold in DataFold:
-        ReactionDataset.save_samples_to_file(data_dir=tmp_path, fold=fold, samples=samples)
+        DiskReactionDataset.save_samples_to_file(data_dir=tmp_path, fold=fold, samples=samples)
 
-    # Now try to load the data we just saved.
-    dataset = DiskReactionDataset(tmp_path, sample_cls=sample_cls)
+    for load_format in [None, DataFormat.JSONL]:
+        # Now try to load the data we just saved.
+        dataset = DiskReactionDataset(tmp_path, sample_cls=ReactionSample, data_format=load_format)
 
-    for fold in DataFold:
-        assert list(dataset[fold]) == samples
+        for fold in DataFold:
+            assert list(dataset[fold]) == samples
+
+
+@pytest.mark.parametrize("format", [DataFormat.CSV, DataFormat.SMILES])
+def test_load_external_format(tmp_path: Path, format: DataFormat) -> None:
+    # Example reaction SMILES, purposefully using non-canonical forms of reactants and product.
+    reaction_smiles = (
+        "[cH:1]1[cH:2][c:3]([CH3:4])[cH:5][cH:6][c:7]1Br.B(O)(O)[c:8]1[cH:9][cH:10][c:11]([CH3:12])[cH:13][cH:14]1>>"
+        "[cH:1]1[cH:2][c:3]([CH3:4])[cH:5][cH:6][c:7]1[c:8]2[cH:14][cH:13][c:11]([CH3:12])[cH:10][cH:9]2"
+    )
+
+    filename = DiskReactionDataset.get_filename_suffix(format=format, fold=DataFold.TRAIN)
+    with open(tmp_path / filename, "wt") as f:
+        if format == DataFormat.CSV:
+            writer = csv.DictWriter(f, fieldnames=["id", "class", CSV_REACTION_SMILES_COLUMN_NAME])
+            writer.writeheader()
+            writer.writerow(
+                {"id": 0, "class": "UNK", CSV_REACTION_SMILES_COLUMN_NAME: reaction_smiles}
+            )
+        else:
+            f.write(f"{reaction_smiles}\n")
+
+    for load_format in [None, format]:
+        dataset = DiskReactionDataset(tmp_path, sample_cls=ReactionSample, data_format=load_format)
+        assert dataset.get_num_samples(DataFold.TRAIN) == 1
+
+        samples = list(dataset[DataFold.TRAIN])
+        assert len(samples) == 1
+
+        [sample] = samples
+
+        # After loading, the reactants and products should be in canonical SMILES form, with the
+        # atom mapping removed.
+        assert sample == ReactionSample(
+            reactants=Bag([Molecule("Cc1ccc(Br)cc1"), Molecule("Cc1ccc(B(O)O)cc1")]),
+            products=Bag([Molecule("Cc1ccc(-c2ccc(C)cc2)cc1")]),
+        )
+
+        # The original reaction SMILES should have been saved separately.
+        assert sample.mapped_reaction_smiles == reaction_smiles
+
+
+@pytest.mark.parametrize("format", [DataFormat.JSONL, DataFormat.CSV, DataFormat.SMILES])
+def test_format_detection(tmp_path: Path, format: DataFormat) -> None:
+    other_format = (set(DataFormat) - {format}).pop()
+
+    # Create two files with different extensions, so that it is ambiguous which format we want.
+    (tmp_path / DiskReactionDataset.get_filename_suffix(format, DataFold.TRAIN)).touch()
+    (tmp_path / DiskReactionDataset.get_filename_suffix(other_format, DataFold.TEST)).touch()
+
+    # Loading with automatic resolution should fail.
+    with pytest.raises(ValueError):
+        DiskReactionDataset(data_dir=tmp_path, sample_cls=ReactionSample)
+
+    # Loading with an explicit format should succeed.
+    for f in [format, other_format]:
+        DiskReactionDataset(data_dir=tmp_path, sample_cls=ReactionSample, data_format=f)
+
+    # Loading with an explicit format but no matching files should fail.
+    another_format = (set(DataFormat) - {format, other_format}).pop()
+    with pytest.raises(ValueError):
+        DiskReactionDataset(
+            data_dir=tmp_path, sample_cls=ReactionSample, data_format=another_format
+        )
+
+    # Create another file with the right suffix.
+    (tmp_path / f"raw_{DiskReactionDataset.get_filename_suffix(format, DataFold.TRAIN)}").touch()
+
+    # Loading with an explicit format should now fail due to ambiguity.
+    with pytest.raises(ValueError):
+        DiskReactionDataset(data_dir=tmp_path, sample_cls=ReactionSample, data_format=format)
