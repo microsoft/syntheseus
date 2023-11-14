3a0372849d22a7c0c684e6844076993c09188ce1
Austin Tripp <ajt212@cam.ac.uk>
2023-11-14 16:51:59 +0000

    Add full method to check for starting molecule matches.

--- syntheseus/search/analysis/starting_molecule_match.py
+++ syntheseus/search/analysis/starting_molecule_match.py
@@ -6,7 +6,26 @@ import itertools
 import typing
 from collections.abc import Iterable
 
+from syntheseus.search.chem import Molecule
+from syntheseus.search.graph.and_or import ANDOR_NODE, AndOrGraph, OrNode
+
 T = typing.TypeVar("T")
+
+
+def is_route_with_starting_mols(
+    graph: AndOrGraph,
+    starting_mols: set[Molecule],
+    forbidden_nodes: typing.Optional[set[ANDOR_NODE]] = None,
+) -> bool:
+    """Checks whether there is a route in graph matching the starting mols."""
+    forbidden_nodes = forbidden_nodes or set()
+    return _is_route_with_starting_mols(
+        graph,
+        graph.root_node,
+        starting_mols,
+        forbidden_nodes,
+        _starting_mols_under_each_node(graph),
+    )
 
 
 def partition_set(A: list[T], k: int) -> Iterable[list[list[T]]]:
@@ -60,3 +79,88 @@ def partition_set(A: list[T], k: int) -> Iterable[list[list[T]]]:
             remaining_elements = [x for x in A if x not in first_partition]
             for subsequent_partitions in partition_set(remaining_elements, k - 1):
                 yield [list(first_partition)] + subsequent_partitions
+
+
+def _starting_mols_under_each_node(graph: AndOrGraph) -> dict[ANDOR_NODE, set[Molecule]]:
+    """Get set of molecules reachable under each node in the graph."""
+
+    # Initialize to empty sets, except for nodes with purchasable mols
+    node_to_mols: dict[ANDOR_NODE, set[Molecule]] = {n: set() for n in graph.nodes()}
+    for n in graph.nodes():
+        if isinstance(n, OrNode):
+            node_to_mols[n].add(n.mol)
+
+    # Do passes through all nodes
+    update_happened = True
+    while update_happened:
+        update_happened = False
+        for n in graph.nodes():
+            for c in graph.successors(n):
+                if not (node_to_mols[c] <= node_to_mols[n]):
+                    node_to_mols[n].update(node_to_mols[c])
+                    update_happened = True
+
+    return node_to_mols
+
+
+def _is_route_with_starting_mols(
+    graph: AndOrGraph,
+    start_node: OrNode,
+    starting_mols: set[Molecule],
+    forbidden_nodes: set[ANDOR_NODE],
+    node_to_all_reachable_starting_mols: dict[ANDOR_NODE, set[Molecule]],
+) -> bool:
+    """
+    Recursive method to check whether there is a route in the graph,
+    starting from `start_node` and excluding `forbidden_nodes`,
+    whose leaves and exactly `starting_mols`.
+
+    To prune the search, we use the `node_to_all_reachable_starting_mols` dictionary,
+    which contains the set of all purchasable molecules reachable under each node (not necessarily part of a single route though).
+    We use this to prune the search early: if some molecules cannot be reached at all, there is no point checking whether
+    they might be reachable from a single route.
+    """
+    assert start_node in graph
+
+    # Base case 1: starting mols is empty
+    if len(starting_mols) == 0:
+        return False
+
+    # Base case 2: start node is forbidden
+    if start_node in forbidden_nodes:
+        return False
+
+    # Base case 3: starting are not reachable at all from the start node
+    if not (starting_mols <= node_to_all_reachable_starting_mols[start_node]):
+        return False
+
+    # Base case 4: there is just one starting molecule and this OrNode contains it.
+    if len(starting_mols) == 1 and list(starting_mols)[0] == start_node.mol:
+        return True
+
+    # Main case: the required starting molecules are reachable,
+    # but we just need to check whether they are reachable within a single synthesis route.
+    # We do this by explicitly trying to find this synthesis route.
+    for rxn_child in graph.successors(start_node):
+        # If the starting molecules are not reachable from this reaction child, abort the search
+        if node_to_all_reachable_starting_mols[rxn_child] >= starting_mols:
+            # Also abort search if any grandchildren are forbidden
+            grandchildren = list(graph.successors(rxn_child))
+            if not any(gc in forbidden_nodes for gc in grandchildren):
+                # Main recurisve call: we partition K molecules among N children and check whether
+                for start_mol_partition in partition_set(list(starting_mols), len(grandchildren)):
+                    for gc, allocated_start_mols in zip(grandchildren, start_mol_partition):
+                        assert isinstance(gc, OrNode)
+                        if not _is_route_with_starting_mols(
+                            graph,
+                            gc,
+                            set(allocated_start_mols),
+                            forbidden_nodes | {start_node, rxn_child},
+                            node_to_all_reachable_starting_mols,
+                        ):
+                            break
+                    else:  # i.e. loop finished without breaking
+                        return True
+
+    # If the method has not returned at this point then there is no route
+    return False
--- syntheseus/tests/search/analysis/test_starting_molecule_match.py
+++ syntheseus/tests/search/analysis/test_starting_molecule_match.py
@@ -1,6 +1,8 @@
 import pytest
 
 from syntheseus.search.analysis import starting_molecule_match
+from syntheseus.search.chem import Molecule
+from syntheseus.search.graph.and_or import AndOrGraph
 
 
 @pytest.mark.parametrize(
@@ -73,3 +75,51 @@ def test_partition_set_valid(A, k, expected_partitions):
 def test_partition_set_invalid(A, k):
     with pytest.raises(AssertionError):
         list(starting_molecule_match.partition_set(A, k))
+
+
+class TestStartingMoleculeMatch:
+    @pytest.mark.parametrize(
+        "starting_smiles,expected_ans",
+        [
+            ("COCS", True),  # Is starting molecule
+            ("CO.CS", True),  # One of the routes
+            ("CC", True),  # Another route
+            ("CS.CC", True),  # Another route
+            ("CO.CC", True),  # Can be a route if CO occurs twice and is reacted in one of them
+            ("COCC.CC", False),  # both mols are in graph, but not part of same route
+            ("", False),  # an empty set should always be False
+        ],
+    )
+    def test_small_andorgraph(
+        self, andor_graph_non_minimal: AndOrGraph, starting_smiles: str, expected_ans: bool
+    ):
+        starting_mols = {Molecule(s) for s in starting_smiles.split(".")}
+        assert (
+            starting_molecule_match.is_route_with_starting_mols(
+                andor_graph_non_minimal, starting_mols
+            )
+            == expected_ans
+        )
+
+    @pytest.mark.parametrize(
+        "starting_smiles,expected_ans",
+        [
+            ("CC.COC", True),  # small route, should be in there
+            ("CCCO.O", True),  # another route from docstring
+            ("CCCO.C", True),  # this route exists (although C is not purchasable here)
+            ("CCCOC", True),  # this is just the root node
+            ("", False),  # an empty set should always be False
+            ("C.O", True),  # should be possible to decompose into just C,O
+            ("CCCO.CC", False),  # too many atoms
+        ],
+    )
+    def test_large_andorgraph(
+        self, andor_graph_with_many_routes: AndOrGraph, starting_smiles: str, expected_ans: bool
+    ):
+        starting_mols = {Molecule(s) for s in starting_smiles.split(".")}
+        assert (
+            starting_molecule_match.is_route_with_starting_mols(
+                andor_graph_with_many_routes, starting_mols
+            )
+            == expected_ans
+        )
