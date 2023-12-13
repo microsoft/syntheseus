4bfa3132c32e0b3c8b3542465bfb530c08fae53a
Krzysztof Maziarz <Krzysztof.Maziarz@microsoft.com>
2023-12-13 11:53:45 +0000

    test(models): Extend test_prediction

--- syntheseus/tests/interface/test_models.py
+++ syntheseus/tests/interface/test_models.py
@@ -2,7 +2,7 @@ import math
 
 import numpy as np
 
-from syntheseus.interface.models import Prediction
+from syntheseus.interface.models import Prediction, PredictionList
 from syntheseus.interface.molecule import Molecule
 
 # TODO(kmaziarz): Currently this test mostly checks that importing from `models.py` works, and that
@@ -13,3 +13,11 @@ def test_prediction():
     prediction = Prediction(input=Molecule("C"), output=Molecule("CC"), probability=0.5)
     assert np.isclose(prediction.get_prob(), 0.5)
     assert np.isclose(prediction.get_log_prob(), math.log(0.5))
+
+    other_prediction = Prediction(input=Molecule("N"), output=Molecule("NC=O"), probability=0.5)
+    prediction_list = PredictionList(
+        input=Molecule("C"), predictions=[prediction, other_prediction]
+    )
+
+    assert prediction_list.predictions == [prediction, other_prediction]
+    assert prediction_list.truncated(num_results=1).predictions == [prediction]
