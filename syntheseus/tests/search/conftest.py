2dfd47d5073a1d30c23705bed9bf0b09308572d8
Austin Tripp <ajt212@cam.ac.uk>
2024-02-05 13:35:22 +0000

    Merge reaction tests.

--- syntheseus/tests/search/conftest.py
+++ syntheseus/tests/search/conftest.py
@@ -33,18 +33,6 @@ class RetrosynthesisTask:
 
 
 @pytest.fixture
-def cocs_mol() -> Molecule:
-    """Returns the molecule with smiles 'COCS'"""
-    return Molecule("COCS", make_rdkit_mol=False)
-
-
-@pytest.fixture
-def rxn_cocs_from_co_cs(cocs_mol: Molecule) -> SingleProductReaction:
-    """Returns a reaction with COCS as the product."""
-    return SingleProductReaction(product=cocs_mol, reactants=Bag([Molecule("CO"), Molecule("CS")]))
-
-
-@pytest.fixture
 def rxn_cs_from_cc() -> SingleProductReaction:
     return SingleProductReaction(product=Molecule("CS"), reactants=Bag([Molecule("CC")]))
 
