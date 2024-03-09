647e779b4e9ccd229759b30044946fe4bb7b5a2e
Austin Tripp <ajt212@cam.ac.uk>
2024-03-09 13:19:25 +0000

    Make basic classes from `interface` importable from top-level (#81)
    Almost any script using syntheseus in a non-trivial way will need to
    import a handful of essential classes from `syntheseus.interface`. This
    PR makes them importable from top-level, allowing users to write
    
    ```python
    from syntheseus import Molecule, Reaction, Bag
    ```
    
    instead of
    
    ```python
    from syntheseus.interface.molecule import Molecule
    from syntheseus.interface.reaction import Reaction
    from syntheseus.interface.bag import Bag
    ```
    
    Imports were also updated in the tutorials.

---
+++ syntheseus/__init__.py
@@ -0,0 +1,14 @@
+from syntheseus.interface.bag import Bag
+from syntheseus.interface.models import BackwardReactionModel, ForwardReactionModel, ReactionModel
+from syntheseus.interface.molecule import Molecule
+from syntheseus.interface.reaction import Reaction, SingleProductReaction
+
+__all__ = [
+    "Molecule",
+    "Reaction",
+    "SingleProductReaction",
+    "Bag",
+    "ReactionModel",
+    "BackwardReactionModel",
+    "ForwardReactionModel",
+]
